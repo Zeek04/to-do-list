d3d9a1bd84bad20ba383845ff3e87997d213ea47
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2023-08-17 21:35:43 -0400

    made changes

---
+++ script.js
@@ -0,0 +1,14 @@
+const box = getElementByID("box");
+const list = getElementByID("list");
+const toDo = getElementByID("ToDo");
+
+document.addEventListener("submit", () => {
+    console.log("Clicked")
+
+})
+
+
+
+//1.Store text from users when submit button pressed.
+//2. text will create a list.
+//3. display the text in the box.

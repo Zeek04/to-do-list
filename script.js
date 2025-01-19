5bf82b37ce2226c2efe5c9dbaab0cc68294268b0
Ezequiel <cruzezequiel81@gmail.com>
2025-01-18 23:38:57 -0500

    Added option to delete on a list item. I aslo customize css

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const listContainer = document.getElementById('list')
 
 
 submitBtn.addEventListener('click', () => {
+    
+    if (textBox.value.trim() === '') {
+        alert('Please enter a task!');
+        return;
+    }
+
     const li = document.createElement('li')
-
     li.textContent = textBox.value;
+    li.classList.add('task-item');
 
+    li.addEventListener('click', () => {
+        li.remove();
+    })
 
     listContainer.append(li)
+
+    textBox.value = ''
 
 })

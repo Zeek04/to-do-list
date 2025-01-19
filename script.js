975d305a01216a033d1374920a87d8c73fff0f0f
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2025-01-19 04:39:35 -0500

    Tasks will now be saved in your local storage.

--- script.js
+++ script.js
@@ -2,24 +2,65 @@ const submitBtn = document.getElementById('submit');
 const textBox = document.getElementById('text')
 const listContainer = document.getElementById('list')
 
+document.addEventListener('DOMContentLoaded', loadTasks);
 
-submitBtn.addEventListener('click', () => {
-    
-    if (textBox.value.trim() === '') {
-        alert('Please enter a task!');
+submitBtn.addEventListener('click', () => {addTask()})
+
+textBox.addEventListener('keydown', (e) => {
+    if(e.key === "Enter"){
+        addTask()
+    }
+})
+
+function addTask(){
+    const task = textBox.value.trim()
+
+    if(task === ''){
+        alert("Please Input A Task");
         return;
     }
 
+    addTaskToList(task)
+    saveTaskToLocalStorage(task)
+    textBox.value = '';
+}
+
+function addTaskToList(task){
     const li = document.createElement('li')
-    li.textContent = textBox.value;
-    li.classList.add('task-item');
+
+    li.textContent = task
 
     li.addEventListener('click', () => {
-        li.remove();
+        li.remove()
+        removeTaskFromLocalStorage()
     })
 
-    listContainer.append(li)
+    listContainer.append(li);
+}
 
-    textBox.value = ''
+function saveTaskToLocalStorage(task){
+    let tasks = getTasksFromLocalStorage()
 
-})
+    tasks.push(task)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks(){
+    let tasks = getTasksFromLocalStorage()
+
+    tasks.forEach((task) => {
+        addTaskToList(task)
+    });
+}
+
+function getTasksFromLocalStorage() {
+    let tasks = localStorage.getItem('tasks');
+    return tasks ? JSON.parse(tasks) : [];
+}
+
+function removeTaskFromLocalStorage(task) {
+    let tasks = getTasksFromLocalStorage();
+    tasks = tasks.filter((t) => t !== task); // Remove the matching task
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
